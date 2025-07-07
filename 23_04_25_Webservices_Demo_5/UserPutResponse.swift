b92b055303b2d624d5989c5b75a4f5c1e86c09e4
aishwarya <ashp17star@gmail.com>
2025-07-07 15:14:16 +0530

    .

---
+++ 23_04_25_Webservices_Demo_5/UserPutResponse.swift
@@ -0,0 +1,12 @@
+//
+//  UserPutResponse.swift
+//  23_04_25_Webservices_Demo_5
+//
+//  Created by Vishal Jagtap on 07/07/25.
+//
+
+struct UserPutResponse : Decodable{
+    var name : String
+    var job : String
+    var updatedAt : String
+}
--- 23_04_25_Webservices_Demo_5/ViewController.swift
+++ 23_04_25_Webservices_Demo_5/ViewController.swift
@@ -14,10 +14,14 @@ class ViewController: UIViewController {
     @IBOutlet var nameTextField: UITextField!
     @IBOutlet var jobLabel: UILabel!
     @IBOutlet var jobTextField: UITextField!
-    var urlString = "https://reqres.in/api/users"
+    var baseUrl = "https://reqres.in"
+    
+    var urlStringForPost = "/api/users"
+    var urlStringForPut = "/api/users/2"
+    var urlStringForDelete = "/api/users/2"
     var url: URL?
     var urlRequest: URLRequest?
-
+    
     var headers: HTTPHeaders =
         ["x-api-key": "reqres-free-v1"]
 
@@ -26,11 +30,57 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        url = URL(string: urlString)
-        urlRequest = URLRequest(url: url!)
+    
     }
-
+    
+    @IBAction func deleteUser(_ sender: UIButton ) {
+        url = URL(string: baseUrl + urlStringForDelete)
+        AF.request(url!,
+                   method: .delete,
+                   encoding: JSONEncoding.default,
+                   headers: headers).response { response in
+            print(response.response!.statusCode)
+            switch response.result{
+                case .success(let data):
+                    print("Successfully deleted record")
+                case .failure(let error):
+                    print(error)
+            }
+        }
+    }
+    
+    //PUT API  -- update user
+    
+    @IBAction func updateUserBtn(_ sender: UIButton) {
+        url = URL(string: baseUrl + urlStringForPut)
+        
+        extractedName = self.nameTextField.text
+        extractedJob = self.jobTextField.text
+        
+        var extractedParamerters = [
+                                    "name" : extractedName!,
+                                    "job" : extractedJob!
+                                ]
+        
+        AF.request(url!,
+                   method: .put,
+                   parameters: extractedParamerters,
+                   encoding: JSONEncoding.default,
+                   headers: headers).response { response in
+            switch response.result{
+                case .success(let data) :
+                    let putResponse = try! JSONDecoder().decode(UserPutResponse.self, from: data!)
+                    print(putResponse)
+                case .failure(let error) :
+                    print(error)
+            }
+        }
+    }
+    
+    //POST API -- post user
     @IBAction func loginBtn(_ sender: UIButton) {
+        url = URL(string: baseUrl + urlStringForPost)
+        
         extractedName = self.nameTextField.text
         extractedJob = self.jobTextField.text
 
